080ba2429df022a78646a3878fe8f20d37f2d7d7
dervovan <dervovan@gmail.com>
2024-02-23 23:02:35 +0300

    enable localhost cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
